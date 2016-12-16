2b6a215439259b2909b7377ff0cfe7c003d44663
Tuetuopay <tuetuopay@me.com>
2016-12-16 17:46:02 +0100

    Implemented the textRenderer

---
+++ GFX/TextRenderer.cpp
@@ -0,0 +1,42 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Tuetuopay
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ **/
+
+#include "GFX/TextRenderer.h"
+
+namespace GFX {
+
+TextRenderer::TextRenderer() : _shader("text") { _shader.pushUniform("tex", 0); }
+
+void TextRenderer::render(const glm::mat4 &matVP) {
+	// Full MVP matrix
+	glm::mat4 matMVP;
+
+	for (std::shared_ptr<TextLabel> label : _labels) {
+		matMVP = label->model() * matVP;
+		_shader->pushUniform("matMVP", 1, GL_FALSE, &matMVP, 4);
+		label->render();
+	}
+}
+
+}
+
