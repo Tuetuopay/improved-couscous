89719c0c39625ce462bdee1e416ebbd7d5d5faf1
U-DESKTOP-4BNACSG\Alexis <tuetuopay@me.com>
2017-01-03 00:47:19 +0100

    Disabled mip mapping for the textures

--- GFX/GL/Texture.cpp
+++ GFX/GL/Texture.cpp
@@ -36,7 +36,7 @@ GLuint textureFromTGA(std::shared_ptr<GFX::TGA> tga) {
 	glTexImage2D(GL_TEXTURE_2D, 0, tga->bpp == 24 ? GL_RGB : GL_RGBA, tga->w, tga->h,
 	             0, tga->bpp == 24 ? GL_BGR : GL_BGRA, GL_UNSIGNED_BYTE, tga->data.get());
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glGenerateMipmap(GL_TEXTURE_2D);
 
 	return tex;
