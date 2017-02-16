8eb37ba7556378f24beb4736aa3b0625962865d7
Tuetuopay <tuetuopay@me.com>
2017-02-16 19:35:12 +0100

    Proper VSync delay with SDL

--- src/GFX/Window.cpp
+++ src/GFX/Window.cpp
@@ -72,6 +72,7 @@ Window::Window(const std::string &title, const int w, const int h)
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetSwapInterval(1);
 	SDL_GL_CreateContext(_window);
 
 	// Get pixel size
